ef25a70e61c21ef35f933de1850ccbda8c6c2e89
Ove98 <ove-henrik.lm@hotmail.com>
2013-12-21 02:04:52 -1200

    Added full permission support.

--- src/me/ove/bukkit/CBMCSuite/Commands/HubCommand.java
+++ src/me/ove/bukkit/CBMCSuite/Commands/HubCommand.java
@@ -47,9 +47,9 @@ public class HubCommand implements CommandExecutor {
 	        else if(args.length == 1) {
 	        	if(args[0].equalsIgnoreCase("Help")) {
 		        	p.sendMessage(pc2+ "[   CBMCSuite Help   ]");
-		        	p.sendMessage(pc2+ "/CBMCSuite" +pc1+ " - Displays important plugin news.");
-		        	p.sendMessage(pc2+ "/Hub Version" +pc1+ " - Displays the current version of the plugin.");
-		        	p.sendMessage(pc2+ "/Hub Information" +pc1+ " - Displays some info about this plugin.");
+		        	if(p.hasPermission("cbmc.news") || p.hasPermission("cbmc.dev")) {p.sendMessage(pc2+ "/CBMCSuite" +pc1+ " - Displays important plugin news.");}
+		        	if(p.hasPermission("cbmc.version")) {p.sendMessage(pc2+ "/Hub Version" +pc1+ " - Displays the current version of the plugin.");}
+		        	if(p.hasPermission("cbmc.info")) {p.sendMessage(pc2+ "/Hub Information" +pc1+ " - Displays some info about this plugin.");}
 		        	p.sendMessage(pc2+ "/Hub" +pc1+ " - ");
 		        	p.sendMessage(pc2+ "/Hub" +pc1+ " - ");
 		        	p.sendMessage(pc2+ "/Hub" +pc1+ " - ");
@@ -59,12 +59,16 @@ public class HubCommand implements CommandExecutor {
 	        	}
 	        	
 	        	else if(args[0].equalsIgnoreCase("Version") || args[0].equalsIgnoreCase("Ver")) {
-	        		p.sendMessage(x+ "Current Version: " +ChatColor.ITALIC+ manager.getConfig().getDouble("Plugin.Version"));
+	        		if(p.hasPermission("cbmc.version")) {
+	        			p.sendMessage(x+ "Current Version: " +ChatColor.ITALIC+ manager.getConfig().getDouble("Plugin.Version"));
+	        		}
 	        	}
 	        	
 	        	else if(args[0].equalsIgnoreCase("Information") || args[0].equalsIgnoreCase("Info")) {
-	        		p.sendMessage(x+ ChatColor.GRAY + "Created By Ove98 & CreeperAtonomy");
-	        		p.sendMessage(x+ "CBMC Hub is a plugin designed for the CB-MC server, it's the newest Hub plugin that enhances your server experience.");
+	        		if(p.hasPermission("cbmc.info")) {
+	        			p.sendMessage(x+ "Created By Ove98 & CreeperAtonomy");
+	        			p.sendMessage(x+ "CBMC Hub is a plugin designed for the CB-MC server, it's the newest Hub plugin that enhances your server experience.");
+	        		}
 	        	}
 	        	
 	        	
