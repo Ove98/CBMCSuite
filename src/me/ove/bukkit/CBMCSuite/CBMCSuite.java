af987d4c7d0be52d4068f612fef9894b875def42
Ove98 <ove-henrik.lm@hotmail.com>
2013-12-20 23:47:16 -1200

    ^^

---
+++ src/me/ove/bukkit/CBMCSuite/CBMCSuite.java
@@ -0,0 +1,23 @@
+package me.ove.bukkit.CBMCSuite;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+//
+// CBMCSuite By Ove98
+// Copyright (c) OveHenrik 2013
+//
+
+public class CBMCSuite extends JavaPlugin {
+	CBMCSuiteManager manager = CBMCSuiteManager.getInstance();
+	public static final Logger log = Logger.getLogger("Minecraft");
+	
+	public void onEnable() {
+		
+	}
+	
+	
+	public void onDisable() {
+		
+	}
+}
